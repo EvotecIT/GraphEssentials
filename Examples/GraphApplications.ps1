7b2cd7a0d652577f6accba114b9117eb37764ae4
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-08-23 09:32:43 +0200

    Example

---
+++ Examples/GraphApplications.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module .\GraphEssentials.psd1 -Force
+
+Connect-MgGraph -Scopes Application.ReadWrite.All
+
+$Applications = Get-MyApp
+$ApplicationsPassword = Get-MyAppCredentials
+Show-MyApp -FilePath $PSScriptRoot\Reports\Applications.html -Show
