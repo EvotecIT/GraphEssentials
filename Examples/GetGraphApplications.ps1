f2b983a66507551f22f45c7678142396531415a3
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-10-04 23:02:07 +0200

    Update examples

---
+++ Examples/GetGraphApplications.ps1
@@ -0,0 +1,8 @@
+﻿Import-Module .\GraphEssentials.psd1 -Force
+
+Connect-MgGraph -Scopes Application.ReadWrite.All
+
+$Applications = Get-MyApp
+$ApplicationsPassword = Get-MyAppCredentials
+$Applications | Format-Table
+$ApplicationsPassword | Format-Table *
