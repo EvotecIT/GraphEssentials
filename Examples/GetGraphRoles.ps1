9a7994e19aa4a504439c10fd02eb466b9213e94a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-11-09 13:40:18 +0100

    Add examples

---
+++ Examples/GetGraphRoles.ps1
@@ -0,0 +1,20 @@
+﻿Import-Module .\GraphEssentials.psd1 -Force
+
+# $Modules = @(
+#     'Microsoft.Graph.Identity.DirectoryManagement'
+#     'Microsoft.Graph.Authentication'
+#     'Microsoft.Graph.Users'
+#     'Microsoft.Graph.DeviceManagement.Enrolment'
+#     'Microsoft.Graph.Applications'
+# )
+
+Connect-MgGraph -Scopes Application.ReadWrite.All, AccessReview.Read.All, AdministrativeUnit.Read.All, 'User.Read.All', RoleManagement.Read.Directory, Directory.Read.All, EntitlementManagement.Read.All
+
+$Report = Get-MyRole -OnlyWithMembers -Verbose
+$Report | Format-Table
+
+$ReportUsers = Get-MyRoleUsers -OnlyWithRoles -Verbose
+$ReportUsers | Format-Table
+
+$ReportUsers = Get-MyRoleUsers -OnlyWithRoles -RolePerColumn -Verbose
+$ReportUsers | Format-Table *
