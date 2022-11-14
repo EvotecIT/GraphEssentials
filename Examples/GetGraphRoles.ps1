714fa8b3fb3aecdccd0ca5d1f907f78d4e5c277d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-11-14 22:33:16 +0100

    Update examples

--- Examples/GetGraphRoles.ps1
+++ Examples/GetGraphRoles.ps1
@@ -1,12 +1,18 @@
-﻿Import-Module .\GraphEssentials.psd1 -Force
+﻿#Clear-Host
+Import-Module .\GraphEssentials.psd1 -Force
 
-Connect-MgGraph -Scopes Application.ReadWrite.All, AccessReview.Read.All, AdministrativeUnit.Read.All, 'User.Read.All', RoleManagement.Read.Directory, Directory.Read.All, EntitlementManagement.Read.All
+Connect-MgGraph -Scopes DeviceManagementRBAC.Read.All, Application.ReadWrite.All, AccessReview.Read.All, AdministrativeUnit.Read.All, 'User.Read.All', RoleManagement.Read.Directory, Directory.Read.All, EntitlementManagement.Read.All
 
 $Report = Get-MyRole -OnlyWithMembers -Verbose
-$Report | Format-Table
+$Report | Format-Table *
 
 $ReportUsers = Get-MyRoleUsers -OnlyWithRoles -Verbose
-$ReportUsers | Format-Table
+$ReportUsers | Format-Table *
 
 $ReportUsers = Get-MyRoleUsers -OnlyWithRoles -RolePerColumn -Verbose
 $ReportUsers | Format-Table *
+
+#Install-Modue Microsoft.Graph.Teams -Force -Verbose
+#Install-Module Microsoft.Graph.Intune -Force -Verbose
+
+#Get-DeviceManagement_RoleAssignments
