102639ea3e2854c8d1db1a7a2e2ac8f5ca359ba6
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-01-15 18:49:06 +0100

    add table conditions

--- Private/Configuration.UsersPerLicense.ps1
+++ Private/Configuration.UsersPerLicense.ps1
@@ -16,13 +16,20 @@
     Solution   = {
         if ($Script:Reporting['UsersPerLicense']['Data']) {
             New-HTMLTable -DataTable $Script:Reporting['UsersPerLicense']['Data'] -Filtering {
-                # New-HTMLTableCondition -Name 'DescriptionWithEmail' -Operator eq -Value $true -ComparisonType string -BackgroundColor SpringGreen -FailBackgroundColor Salmon
-                # New-HTMLTableCondition -Name 'KeysCount' -Operator gt -Value 1 -ComparisonType number -BackgroundColor GoldenFizz
-                # New-HTMLTableCondition -Name 'KeysCount' -Operator eq -Value 0 -ComparisonType number -BackgroundColor Salmon -Row
-                # New-HTMLTableCondition -Name 'KeysCount' -Operator eq -Value 1 -ComparisonType number -BackgroundColor SpringGreen
-                # New-HTMLTableCondition -Name 'Expired' -Operator eq -Value "No" -ComparisonType string -BackgroundColor SpringGreen
-                # New-HTMLTableCondition -Name 'Expired' -Operator eq -Value "Yes" -ComparisonType string -BackgroundColor Salmon -Row
-                # New-HTMLTableCondition -Name 'Expired' -Operator eq -Value "Not available" -ComparisonType string -BackgroundColor Salmon -Row
+                New-HTMLTableCondition -Name 'AccountEnabled' -Operator eq -Value $false -ComparisonType string -BackgroundColor Salmon
+                New-HTMLTableCondition -Name 'AccountEnabled' -Operator eq -Value $true -ComparisonType string -BackgroundColor SpringGreen
+
+                foreach ($Name in $Script:Reporting['UsersPerLicense']['Data'][0].PSObject.Properties.Name) {
+                    if ($Name -notin 'DisplayName', 'Id', 'Enabled', 'UserPrincipalName', 'AccountEnabled', 'Mail' , 'Manager', 'LicensesStatus', 'LicensesErrors', 'Surname', 'LastPasswordChangeDateTime', 'GivenName', 'JobTitle') {
+                        New-HTMLTableCondition -Name $Name -Operator eq -Value 'Direct' -ComparisonType string -BackgroundColor GoldenFizz
+                        New-HTMLTableCondition -Name $Name -Operator eq -Value 'Group' -ComparisonType string -BackgroundColor LightGreen
+                        New-HTMLTableConditionGroup -Conditions {
+                            New-HTMLTableCondition -Name $Name -Operator ne -Value 'Group' -ComparisonType string
+                            New-HTMLTableCondition -Name $Name -Operator ne -Value 'Direct' -ComparisonType string
+                            New-HTMLTableCondition -Name $Name -Operator ne -Value '' -ComparisonType string
+                        } -Logic AND -BackgroundColor Orange -HighlightHeaders $Name
+                    }
+                }
             } -ScrollX
         }
     }
