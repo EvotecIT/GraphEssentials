6457ca4918a96eabcdd20fbae89e38f6f2cc6259
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-01-15 18:49:26 +0100

    Add helper function

--- Public/Get-MgToken.ps1
+++ Public/Get-MgToken.ps1
@@ -1,9 +1,14 @@
 ﻿function Get-MgToken {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = 'Domain')]
     param(
+        [Parameter(ParameterSetName = 'TenantID', Mandatory)]
+        [Parameter(ParameterSetName = 'Domain', Mandatory)]
         [alias('ApplicationID')][string] $ClientID,
+        [Parameter(ParameterSetName = 'TenantID', Mandatory)]
+        [Parameter(ParameterSetName = 'Domain', Mandatory)]
         [string] $ClientSecret,
-        [string] $TenantID
+        [Parameter(ParameterSetName = 'TenantID', Mandatory)][string] $TenantID,
+        [Parameter(ParameterSetName = 'Domain', Mandatory)][string] $Domain
     )
     $Body = @{
         Grant_Type    = "client_credentials"
@@ -11,6 +16,15 @@
         Client_Id     = $ClientID
         Client_Secret = $ClientSecret
     }
-    $connection = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$tenantid/oauth2/v2.0/token" -Method POST -Body $Body
+
+    if ($TenantID) {
+        $Tenant = $TenantID
+    } elseif ($Domain) {
+        $Tenant = Get-O365TenantID -Domain $Domain
+    }
+    if (-not $Tenant) {
+        throw "Get-MgToken - Unable to get Tenant ID"
+    }
+    $connection = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$Tenant/oauth2/v2.0/token" -Method POST -Body $Body
     $connection.access_token
 }
