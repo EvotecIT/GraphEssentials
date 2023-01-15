1a2744b45f569a27766a4d565e82385673a44a9d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-01-15 15:39:00 +0100

    updated example

--- Examples/RemoveGraphApplicationsPasswords.ps1
+++ Examples/RemoveGraphApplicationsPasswords.ps1
@@ -2,10 +2,7 @@
 
 Connect-MgGraph -Scopes Application.ReadWrite.All
 
-#Remove-MyAppCredentials -Expired -Verbose -WhatIf -ApplicationName 'TeamsAppDelegation'
-
-#Remove-MyAppCredentials -Verbose -WhatIf -DisplayNameCredentials "O:*" -ApplicationName "Emails Graph"
-
-#Remove-MyAppCredentials -Verbose -WhatIf -Expired
-
+Remove-MyAppCredentials -Expired -Verbose -WhatIf -ApplicationName 'TeamsAppDelegation'
+Remove-MyAppCredentials -Verbose -WhatIf -DisplayNameCredentials "O:*" -ApplicationName "Emails Graph"
+Remove-MyAppCredentials -Verbose -WhatIf -Expired
 Remove-MyAppCredentials -Verbose -DisplayNameCredentials "przemyslaw.klys*" -ApplicationName "sp-gy-ufi-dd" -WhatIf
