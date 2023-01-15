cbc431c2b0c774f467ebac839a72854ca6f51d9c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-01-15 15:24:07 +0100

    Basic Get-MgToken

---
+++ Public/Get-MgToken.ps1
@@ -0,0 +1,16 @@
+﻿function Get-MgToken {
+    [CmdletBinding()]
+    param(
+        [alias('ApplicationID')][string] $ClientID,
+        [string] $ClientSecret,
+        [string] $TenantID
+    )
+    $Body = @{
+        Grant_Type    = "client_credentials"
+        Scope         = "https://graph.microsoft.com/.default"
+        Client_Id     = $ClientID
+        Client_Secret = $ClientSecret
+    }
+    $connection = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$tenantid/oauth2/v2.0/token" -Method POST -Body $Body
+    $connection.access_token
+}
