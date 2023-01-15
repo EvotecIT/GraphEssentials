cc540b4adb71a7d9ac41c29e672aae5d4e705044
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-01-15 09:20:13 +0100

    Add example

---
+++ Examples/GetLicenses.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module .\GraphEssentials.psd1 -Force
+
+Connect-MgGraph -Scopes Application.ReadWrite.All, AccessReview.Read.All, AdministrativeUnit.Read.All, 'User.Read.All', RoleManagement.Read.Directory, Directory.Read.All, EntitlementManagement.Read.All
+
+Get-MyLicense | Format-Table
+
+Get-MyLicense -Internal | Format-Table
