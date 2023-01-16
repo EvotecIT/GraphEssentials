b5c7aedc10a7021941af661a0f0f2e80050a73ba
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-01-16 08:32:32 +0100

    add example

---
+++ Examples/ShowGraphLicenses.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module .\GraphEssentials.psd1 -Force
+
+$AccessToken = Get-MgToken -Domain 'evotec.pl' -ClientID '206245ab-da91-490c-8e69-7a7f3db6df25' -ClientSecret $Env:GraphClientSecret
+
+Connect-MgGraph -AccessToken $AccessToken
+
+Invoke-MyGraphEssentials -Type Users, UsersPerLicense, UsersPerServicePlan, Licenses -FilePath $PSScriptRoot\Reports\GraphEssentials.html -Verbose #-SplitReports
