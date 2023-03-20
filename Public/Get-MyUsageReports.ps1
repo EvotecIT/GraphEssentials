e9b0b0859ac11bf738874d7a7be096d16ed47e61
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-03-20 18:24:05 +0100

    Add verbose message, fix error handling

--- Public/Get-MyUsageReports.ps1
+++ Public/Get-MyUsageReports.ps1
@@ -76,9 +76,11 @@
         )] $Report
     )
     if ($Period) {
+        Write-Verbose -Message "Get-MyUsageReports - Report: $Report, Period: $Period"
         $DayPeriod = "D$Period"
         $Type = "(period='$DayPeriod')"
     } else {
+        Write-Verbose -Message "Get-MyUsageReports - Report: $Report, DateTime: $DateTime"
         # last 30 days YYYY-MM-DD
         $DateTimeConverted = $DateTime.ToUniversalTime().ToString('yyyy-MM-dd')
         $Type = "(date=$DateTimeConverted)"
@@ -119,17 +121,21 @@
         $ErrorDefault = $_.Exception.Message
         $ErrorDetails = $_.ErrorDetails.Message
         # get only the last line of the error message
-        $ErrorDetails = $ErrorDetails.Split("`n")[-1]
-        Try {
-            $ErrorJSON = $ErrorDetails | ConvertFrom-Json -ErrorAction Stop
-        } catch {
-            Write-Warning -Message "Get-MyUsageReports - Error when requesting data for $Report $Type. Error: $ErrorDefault"
-        }
-        try {
-            $NestedError = $ErrorJSON.error.message | ConvertFrom-Json -ErrorAction Stop
-            $FinalErrorMessage = $NestedError.error.message
-            Write-Warning -Message "Get-MyUsageReports - Error when requesting data for $Report $Type. Error: $FinalErrorMessage"
-        } catch {
+        if ($ErrorDetails) {
+            $ErrorDetails = $ErrorDetails.Split("`n")[-1]
+            Try {
+                $ErrorJSON = $ErrorDetails | ConvertFrom-Json -ErrorAction Stop
+            } catch {
+                Write-Warning -Message "Get-MyUsageReports - Error when requesting data for $Report $Type. Error: $ErrorDefault"
+            }
+            try {
+                $NestedError = $ErrorJSON.error.message | ConvertFrom-Json -ErrorAction Stop
+                $FinalErrorMessage = $NestedError.error.message
+                Write-Warning -Message "Get-MyUsageReports - Error when requesting data for $Report $Type. Error: $FinalErrorMessage"
+            } catch {
+                Write-Warning -Message "Get-MyUsageReports - Error when requesting data for $Report $Type. Error: $ErrorDefault"
+            }
+        } else {
             Write-Warning -Message "Get-MyUsageReports - Error when requesting data for $Report $Type. Error: $ErrorDefault"
         }
     }
