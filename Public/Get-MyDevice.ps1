0b1fc665cd02544857c6cf5b865d254633db76a8
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-04-15 09:32:40 +0200

    Add my device report

---
+++ Public/Get-MyDevice.ps1
@@ -0,0 +1,44 @@
+﻿function Get-MyDevice {
+    [cmdletBinding()]
+    param(
+
+    )
+
+    $TrustTypes = @{
+        'ServerAD'  = 'Hybrid AzureAD'
+        'AzureAD'   = 'AzureAD joined'
+        'Workplace' = 'AzureAD registered'
+    }
+
+    $Today = Get-Date
+    $Devices = Get-MgDevice -All
+    foreach ($Device in $Devices) {
+        if ($Device.ApproximateLastSignInDateTime) {
+            $LastSeenDays = $( - $($Device.ApproximateLastSignInDateTime - $Today).Days)
+        } else {
+            $LastSeenDays = $null
+        }
+        if ($Device.OnPremisesLastSyncDateTime) {
+            $LastSynchronizedDays = $( - $($Device.OnPremisesLastSyncDateTime - $Today).Days)
+        } else {
+            $LastSynchronizedDays = $null
+        }
+        [PSCustomObject] @{
+            Name                   = $Device.DisplayName
+            Id                     = $Device.Id
+            Enabled                = $Device.AccountEnabled
+            DeviceId               = $Device.DeviceId
+            OperatingSystem        = $Device.OperatingSystem
+            OperatingSystemVersion = $Device.OperatingSystemVersion
+            TrustType              = $TrustTypes[$Device.TrustType]
+            FirstSeen              = $Device.AdditionalProperties.registrationDateTime
+            LastSeen               = $Device.ApproximateLastSignInDateTime
+            LastSeenDays           = $LastSeenDays
+            IsSynchronized         = if ($Device.OnPremisesSyncEnabled) { $true } else { $false }
+            LastSynchronized       = $Device.OnPremisesLastSyncDateTime
+            LastSynchronizedDays   = $LastSynchronizedDays
+            IsCompliant            = $Device.IsCompliant
+            IsManaged              = $Device.IsManaged
+        }
+    }
+}
