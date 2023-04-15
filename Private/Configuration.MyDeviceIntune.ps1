61a96b79d94d2aeae02625ad005dfa09b27c6755
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-04-15 09:31:40 +0200

    Add html report for my device intune

---
+++ Private/Configuration.MyDeviceIntune.ps1
@@ -0,0 +1,23 @@
+﻿$Script:DevicesIntune = [ordered] @{
+    Name       = 'Azure Active Directory Devices Intune'
+    Enabled    = $true
+    Execute    = {
+        Get-MyDeviceIntune
+    }
+    Processing = {
+
+    }
+    Summary    = {
+
+    }
+    Variables  = @{
+
+    }
+    Solution   = {
+        if ($Script:Reporting['DevicesIntune']['Data']) {
+            New-HTMLTable -DataTable $Script:Reporting['DevicesIntune']['Data'] -Filtering {
+
+            } -ScrollX
+        }
+    }
+}
