2959fc1f6143f806226b495ff0dafae6a76b9704
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-06-21 11:01:08 +0200

    Teams report

---
+++ Private/Configuration.Teams.ps1
@@ -0,0 +1,23 @@
+﻿$Script:Teams = [ordered] @{
+    Name       = 'Microsoft Teams Report'
+    Enabled    = $true
+    Execute    = {
+        Get-MyTeam
+    }
+    Processing = {
+
+    }
+    Summary    = {
+
+    }
+    Variables  = @{
+
+    }
+    Solution   = {
+        if ($Script:Reporting['Teams']['Data']) {
+            New-HTMLTable -DataTable $Script:Reporting['Teams']['Data'] -Filtering {
+
+            } -ScrollX
+        }
+    }
+}
