c6030a22a86df025158c88088781898408f3a135
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-09-01 11:18:18 +0200

    Improve html report

--- Public/Show-MyApp.ps1
+++ Public/Show-MyApp.ps1
@@ -6,26 +6,41 @@
         [switch] $ShowHTML
     )
 
+    $Script:Reporting = [ordered] @{}
+    $Script:Reporting['Version'] = Get-GitHubVersion -Cmdlet 'Invoke-MyGraphEssentials' -RepositoryOwner 'evotecit' -RepositoryName 'GraphEssentials'
+
     $Applications = Get-MyApp
     $ApplicationsPassword = Get-MyAppCredentials
 
     New-HTML {
-        New-HTMLTableOption -DataStore JavaScript -BoolAsString
+        New-HTMLTabStyle -BorderRadius 0px -TextTransform capitalize -BackgroundColorActive SlateGrey
+        New-HTMLSectionStyle -BorderRadius 0px -HeaderBackGroundColor Grey -RemoveShadow
+        New-HTMLPanelStyle -BorderRadius 0px
+        New-HTMLTableOption -DataStore JavaScript -BoolAsString -ArrayJoinString ', ' -ArrayJoin
 
-        New-HTMLSection -Invisible {
-            New-HTMLSection -HeaderText "Applications" {
-                New-HTMLTable -DataTable $Applications -Filtering {
-                    New-TableEvent -ID 'TableAppsCredentials' -SourceColumnID 1 -TargetColumnID 1
-                } -DataStore JavaScript -DataTableID "TableApps"
+        New-HTMLHeader {
+            New-HTMLSection -Invisible {
+                New-HTMLSection {
+                    New-HTMLText -Text "Report generated on $(Get-Date)" -Color Blue
+                } -JustifyContent flex-start -Invisible
+                New-HTMLSection {
+                    New-HTMLText -Text "GraphEssentials - $($Script:Reporting['Version'])" -Color Blue
+                } -JustifyContent flex-end -Invisible
             }
-            New-HTMLSection -HeaderText 'Applications Credentials' {
-                New-HTMLTable -DataTable $ApplicationsPassword -Filtering {
-                    New-HTMLTableCondition -Name 'DaysToExpire' -Value 30 -Operator 'ge' -BackgroundColor Conifer -ComparisonType number
-                    New-HTMLTableCondition -Name 'DaysToExpire' -Value 30 -Operator 'lt' -BackgroundColor Orange -ComparisonType number
-                    New-HTMLTableCondition -Name 'DaysToExpire' -Value 5 -Operator 'lt' -BackgroundColor Red -ComparisonType number
-                    New-HTMLTableCondition -Name 'Expired' -Value $true -ComparisonType string -BackgroundColor Salmon -FailBackgroundColor Conifer
-                } -DataStore JavaScript -DataTableID "TableAppsCredentials"
-            }
+        }
+
+        New-HTMLSection -HeaderText "Applications" {
+            New-HTMLTable -DataTable $Applications -Filtering {
+                New-TableEvent -ID 'TableAppsCredentials' -SourceColumnID 0 -TargetColumnID 0
+            } -DataStore JavaScript -DataTableID "TableApps" -PagingLength 5 -ScrollX
+        }
+        New-HTMLSection -HeaderText 'Applications Credentials' {
+            New-HTMLTable -DataTable $ApplicationsPassword -Filtering {
+                New-HTMLTableCondition -Name 'DaysToExpire' -Value 30 -Operator 'ge' -BackgroundColor Conifer -ComparisonType number
+                New-HTMLTableCondition -Name 'DaysToExpire' -Value 30 -Operator 'lt' -BackgroundColor Orange -ComparisonType number
+                New-HTMLTableCondition -Name 'DaysToExpire' -Value 5 -Operator 'lt' -BackgroundColor Red -ComparisonType number
+                New-HTMLTableCondition -Name 'Expired' -Value $true -ComparisonType string -BackgroundColor Salmon -FailBackgroundColor Conifer
+            } -DataStore JavaScript -DataTableID "TableAppsCredentials" -ScrollX
         }
     } -ShowHTML:$ShowHTML.IsPresent -FilePath $FilePath -Online:$Online.IsPresent
 }
