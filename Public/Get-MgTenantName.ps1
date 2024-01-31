623393634707f5ae983768afb9f30c31cf0b4f2e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-01-31 12:27:03 +0100

    Add new cmd

---
+++ Public/Get-MgTenantName.ps1
@@ -0,0 +1,37 @@
+﻿function Get-MgTenantName {
+    <#
+    .SYNOPSIS
+    Get Tenant Name from Tenant ID or Domain Name
+
+    .DESCRIPTION
+    Get Tenant Name from Tenant ID or Domain Name
+
+    .PARAMETER TenantID
+    Provide the Tenant ID of the Tenant
+
+    .PARAMETER DomainName
+    Provide the Domain Name of the Tenant
+
+    .EXAMPLE
+    Get-MgTenantName -TenantID 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
+
+    .EXAMPLE
+    Get-MgTenantName -DomainName 'contoso.com'
+
+    .NOTES
+    General notes
+    #>
+    [CmdletBinding(DefaultParameterSetName = 'TenantID')]
+    param(
+        [Parameter(Mandatory, ParameterSetName = 'TenantID')][string] $TenantID,
+        [Parameter(Mandatory, ParameterSetName = 'DomainName')][string] $DomainName
+    )
+
+    if ($TenantID) {
+        $Data = Invoke-MgRestMethod -Method GET -Uri "https://graph.microsoft.com/beta/tenantRelationships/findTenantInformationByTenantId(tenantId='$TenantID')"
+        $Data
+    } elseif ($DomainName) {
+        $Data = Invoke-MgRestMethod -Method GET -Uri "https://graph.microsoft.com/beta/tenantRelationships/findTenantInformationByDomainName(domainName='$DomainName')"
+        $Data
+    }
+}
