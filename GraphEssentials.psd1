4d1698e9884fe3da0d3df7478cea4853ad39c2ea
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-05-09 13:55:59 +0200

    new function

--- GraphEssentials.psd1
+++ GraphEssentials.psd1
@@ -6,20 +6,19 @@
     CompatiblePSEditions = @('Desktop', 'Core')
     Copyright            = '(c) 2011 - 2024 Przemyslaw Klys @ Evotec. All rights reserved.'
     Description          = 'GraphEssentials is a PowerShell module that helps with Office 365 / Azure AD using mostly Graph'
-    FunctionsToExport    = @('Get-MgToken', 'Get-MyApp', 'Get-MyAppCredentials', 'Get-MyDevice', 'Get-MyDeviceIntune', 'Get-MyLicense', 'Get-MyRole', 'Get-MyRoleUsers', 'Get-MyTeam', 'Get-MyTenantName', 'Get-MyUsageReports', 'Get-MyUser', 'Invoke-MyGraphEssentials', 'Invoke-MyGraphUsageReports', 'New-MyApp', 'New-MyAppCredentials', 'Remove-MyAppCredentials', 'Send-MyApp', 'Show-MyApp')
+    FunctionsToExport    = @('Get-MgToken', 'Get-MyApp', 'Get-MyAppCredentials', 'Get-MyDefenderHealthIssues', 'Get-MyDevice', 'Get-MyDeviceIntune', 'Get-MyLicense', 'Get-MyRole', 'Get-MyRoleUsers', 'Get-MyTeam', 'Get-MyTenantName', 'Get-MyUsageReports', 'Get-MyUser', 'Invoke-MyGraphEssentials', 'Invoke-MyGraphUsageReports', 'New-MyApp', 'New-MyAppCredentials', 'Remove-MyAppCredentials', 'Send-MyApp', 'Show-MyApp')
     GUID                 = '75ef812f-6d8e-4898-81bb-8029e0560ef3'
-    ModuleVersion        = '0.0.35'
+    ModuleVersion        = '0.0.36'
     PowerShellVersion    = '5.1'
     PrivateData          = @{
         PSData = @{
-            ExternalModuleDependencies = @('Microsoft.PowerShell.Utility', 'Microsoft.PowerShell.Management', 'Microsoft.PowerShell.Security')
-            Tags                       = @('Windows')
+            Tags = @('Windows')
         }
     }
     RequiredModules      = @(@{
             Guid          = 'ee272aa8-baaa-4edf-9f45-b6d6f7d844fe'
             ModuleName    = 'PSSharedGoods'
-            ModuleVersion = '0.0.276'
+            ModuleVersion = '0.0.289'
         }, @{
             Guid          = '0b0ba5c5-ec85-4c2b-a718-874e55a8bc3f'
             ModuleName    = 'PSWriteColor'
@@ -27,15 +26,15 @@
         }, @{
             Guid          = '467f54f2-44a8-4993-8e75-b96c3e443098'
             ModuleName    = 'Microsoft.Graph.Applications'
-            ModuleVersion = '2.12.0'
+            ModuleVersion = '2.18.0'
         }, @{
             Guid          = 'c767240d-585c-42cb-bb2f-6e76e6d639d4'
             ModuleName    = 'Microsoft.Graph.Identity.DirectoryManagement'
-            ModuleVersion = '2.12.0'
+            ModuleVersion = '2.18.0'
         }, @{
             Guid          = '530fc574-049c-42cc-810e-8835853204b7'
             ModuleName    = 'Microsoft.Graph.Identity.Governance'
-            ModuleVersion = '2.12.0'
+            ModuleVersion = '2.18.0'
         }, @{
             Guid          = 'a7bdf640-f5cb-4acf-9de0-365b322d245c'
             ModuleName    = 'PSWriteHTML'
@@ -43,31 +42,35 @@
         }, @{
             Guid          = 'cb05f22d-85cd-4b42-b99c-b713e0b9fbd3'
             ModuleName    = 'Microsoft.Graph.DeviceManagement.Enrollment'
-            ModuleVersion = '2.12.0'
+            ModuleVersion = '2.18.0'
         }, @{
             Guid          = '71150504-37a3-48c6-82c7-7a00a12168db'
             ModuleName    = 'Microsoft.Graph.Users'
-            ModuleVersion = '2.12.0'
+            ModuleVersion = '2.18.0'
         }, @{
             Guid          = '50bc9e18-e281-4208-8913-c9e1bef6083d'
             ModuleName    = 'Microsoft.Graph.Groups'
-            ModuleVersion = '2.12.0'
+            ModuleVersion = '2.18.0'
         }, @{
             Guid          = '4131557d-8635-4903-9cfd-d59ddef4a597'
             ModuleName    = 'Microsoft.Graph.DeviceManagement'
-            ModuleVersion = '2.12.0'
+            ModuleVersion = '2.18.0'
         }, @{
             Guid          = 'f8619bb2-8640-4d8d-baf5-0829db98fbe2'
             ModuleName    = 'Microsoft.Graph.Teams'
-            ModuleVersion = '2.12.0'
+            ModuleVersion = '2.18.0'
+        }, @{
+            Guid          = '987c645b-9ff9-4398-8963-19739c27f5c3'
+            ModuleName    = 'Microsoft.Graph.Beta.Security'
+            ModuleVersion = '2.18.0'
         }, @{
             Guid          = 'a8752d7b-17c8-41db-b3f9-b8f225de028d'
             ModuleName    = 'O365Essentials'
-            ModuleVersion = '0.0.13'
+            ModuleVersion = '0.0.14'
         }, @{
             Guid          = '2b0ea9f1-3ff1-4300-b939-106d5da608fa'
             ModuleName    = 'Mailozaurr'
             ModuleVersion = '1.0.0'
-        }, 'Microsoft.PowerShell.Utility', 'Microsoft.PowerShell.Management', 'Microsoft.PowerShell.Security')
+        })
     RootModule           = 'GraphEssentials.psm1'
 }
---
+++ Public/Get-MyDefenderHealthIssues.ps1
@@ -0,0 +1,98 @@
+﻿function Get-MyDefenderHealthIssues {
+    <#
+    .SYNOPSIS
+    Retrieves health issues from Microsoft Graph Security API.
+
+    .DESCRIPTION
+    This function queries the Microsoft Graph Security API to retrieve health issues based on various filters such as status, health issue type, severity, and sensor DNS name.
+
+    .PARAMETER Status
+    The status of the health issues to retrieve. Valid values are 'open' and 'closed'.
+
+    .PARAMETER HealthIssueType
+    The type of health issues to retrieve. Valid values are 'global' and 'sensor'.
+
+    .PARAMETER Severity
+    The severity of the health issues to retrieve. Valid values are 'high', 'medium', and 'low'.
+
+    .PARAMETER SensorDNSName
+    The DNS name of the sensor for which to retrieve health issues. Health issues will be filtered to only include those that end with the specified sensor DNS name.
+
+    .PARAMETER All
+    A switch parameter. If specified, all health issues will be retrieved without applying any filters.
+
+    .EXAMPLE
+    Get-MyDefenderHealthIssues -Status 'open' -HealthIssueType 'global' -Severity 'high'
+    Retrieves all open global health issues with high severity.
+
+    .EXAMPLE
+    Get-MyDefenderHealthIssues -SensorDNSName 'contoso.com' -All
+    Retrieves all health issues for sensors whose DNS name ends with 'contoso.com', ignoring other filters.
+
+    .NOTES
+    Examples of Microsoft Graph Security API queries:
+    - See all open health alerts: https://graph.microsoft.com/beta/security/identities/healthIssues?$filter=Status eq 'open'
+    - See open Global health alerts: https://graph.microsoft.com/beta/security/identities/healthIssues?$filter=Status eq 'open' and healthIssueType eq 'global'
+    - See open sensor health alerts: https://graph.microsoft.com/beta/security/identities/healthIssues?$filter=Status eq 'open' and healthIssueType eq 'sensor'
+    - See open health alerts by severity: https://graph.microsoft.com/beta/security/identities/healthIssues?$filter=Status eq 'open' and severity eq 'medium'
+    - See open global health alerts that domain name ends with contoso.com: https://graph.microsoft.com/beta/security/identities/healthissues?$filter=Status eq 'open' and healthIssueType eq 'global' and domainNames/any(s:endswith(s,'contoso.com'))
+    - See open global health alerts that sensor DNS name ends with contoso.com: https://graph.microsoft.com/beta/security/identities/healthissues?$filter=Status eq 'open' and healthIssueType eq 'global' and sensorDNSNames/any(s:endswith(s,'contoso.com'))
+    - See open sensor health alerts with sensor DNS name ends with contoso.corp: https://graph.microsoft.com/beta/security/identities/healthissues?$filter=Status eq 'open' and healthIssueType eq 'sensor' and sensorDNSNames/any(s:endswith(s,'contoso.corp'))
+
+    #>
+    [CmdletBinding()]
+    param(
+        [ValidateSet('open', 'closed')][string] $Status,
+        [ValidateSet('global', 'sensor')][string] $HealthIssueType,
+        [ValidateSet('high', 'medium', 'low')][string] $Severity,
+        [string] $SensorDNSName,
+        [switch] $All
+    )
+
+    $QueryParameters = [ordered] @{
+        All = $All.IsPresent
+    }
+    if ($Status -and $HealthIssueType -and $Severity) {
+        $QueryParameters.Add('$filter', "Status eq '$Status' and healthIssueType eq '$HealthIssueType' and severity eq '$Severity'")
+    } elseif ($Status -and $HealthIssueType) {
+        $QueryParameters.Add('$filter', "Status eq '$Status' and healthIssueType eq '$HealthIssueType'")
+    } elseif ($Status -and $Severity) {
+        $QueryParameters.Add('$filter', "Status eq '$Status' and severity eq '$Severity'")
+    } elseif ($HealthIssueType -and $Severity) {
+        $QueryParameters.Add('$filter', "healthIssueType eq '$HealthIssueType' and severity eq '$Severity'")
+    } elseif ($Status) {
+        $QueryParameters.Add('$filter', "Status eq '$Status'")
+    } elseif ($HealthIssueType) {
+        $QueryParameters.Add('$filter', "healthIssueType eq '$HealthIssueType'")
+    } elseif ($Severity) {
+        $QueryParameters.Add('$filter', "severity eq '$Severity'")
+    }
+    if ($SensorDNSName) {
+        $newFilter = "sensorDNSNames/any(s:endswith(s,'$SensorDNSName'))"
+        if ($QueryParameters.Contains('$filter')) {
+            $QueryParameters['$filter'] += " and $newFilter"
+        } else {
+            $QueryParameters.Add('$filter', $newFilter)
+        }
+    }
+
+    $Data = Get-MgBetaSecurityIdentityHealthIssue @QueryParameters
+    $Data | ForEach-Object {
+        [PSCustomObject] @{
+            DisplayName               = $_.displayName               # : Sensor service failed to start
+            Status                    = $_.status                    # : open
+            Severity                  = $_.severity                  # : high
+            HealthIssueType           = $_.healthIssueType           # : sensor
+            CreatedDateTime           = $_.createdDateTime           # : 2024 - 03 - 31 19:05:55
+            ModifiedDateTime          = $_.lastModifiedDateTime      # : 2024 - 05 - 03 10:55:20
+            SensorDNSNames            = $_.sensorDNSNames            # : { ADRODC.ad.evotec.pl }
+            Description               = $_.description               # : The Sensor service on ADRODC.ad.evotec.pl failed to start. It was last seen running on 05 / 03 / 2024 10:24.
+            Recommendations           = $_.recommendations           # : { Monitor Sensor logs to understand the root cause for Sensor service failure., Refer to the http: / / aka.ms / mdi / troubleshooting. }
+            RecommendedActionCommands = $_.recommendedActionCommands # : {}
+            Id                        = $_.id                        # : cafa6f51-be42 - 418a-968b-7cb529fb18ef
+            DomainNames               = $_.domainNames               # : {}
+            IssueTypeId               = $_.issueTypeId               # :
+            AdditionalInformation     = $_.additionalInformation     # : {}
+        }
+    }
+}
