f7a2c210063cdb8c74d10e4ee803b5fc34485fea
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-12-01 19:25:31 +0100

    Add Get-MyDefenderSensor function to retrieve sensor details and health status

---
+++ Public/Get-MyDefenderSensor.ps1
@@ -0,0 +1,22 @@
+﻿function Get-MyDefenderSensor {
+    [cmdletbinding()]
+    param(
+
+    )
+    Write-Verbose -Message 'Get-MyDefenderSensor - Getting all Sensors'
+    $Sensors = Get-MgBetaSecurityIdentitySensor -All
+    foreach ($Sensor in $Sensors) {
+        [PSCustomObject] @{
+            DisplayName           = $Sensor.displayName
+            DomainName            = $Sensor.domainName
+            DeploymentStatus      = $Sensor.deploymentStatus
+            HealthStatus          = $Sensor.healthStatus
+            OpenHealthIssuesCount = $Sensor.openHealthIssuesCount
+            SensorType            = $Sensor.sensorType
+            Version               = $Sensor.version
+            CreatedDaysAgo        = [math]::Round((Get-Date -Date $Sensor.createdDateTime).TotalDays, 0)
+            CreatedDateTime       = $Sensor.createdDateTime
+            Id                    = $Sensor.id
+        }
+    }
+}
