24d7c50e2464907be8615d0cbfe250660ac0e9d7
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-02-26 08:07:48 +0100

    Add Get-O365TenantID function to retrieve Office 365 Tenant ID by domain name
    - Implemented a new function to find Tenant ID using a domain name
    - Added parameter for domain input
    - Included synopsis, description, and example in the function documentation

---
+++ Private/Get-O365TenantID.ps1
@@ -0,0 +1,26 @@
+﻿function Get-O365TenantID {
+    <#
+    .SYNOPSIS
+    Quick way to find Office 365 Tenant ID by using domain name
+
+    .DESCRIPTION
+    Quick way to find Office 365 Tenant ID by using domain name
+
+    .PARAMETER Domain
+    Domain name to check
+
+    .EXAMPLE
+    Get-O365TenantID -Domain 'evotec.pl'
+
+    .NOTES
+    General notes
+    #>
+    [cmdletbinding()]
+    param(
+        [parameter(Mandatory)][alias('DomainName')][string] $Domain
+    )
+    $Invoke = Invoke-RestMethod "https://login.windows.net/$Domain/.well-known/openid-configuration" -Method GET -Verbose:$false
+    if ($Invoke) {
+        $Invoke.userinfo_endpoint.Split("/")[3]
+    }
+}
