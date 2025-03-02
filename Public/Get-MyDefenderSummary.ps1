f9410ba29f9309c8333629edad790531faf7ec88
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-03-02 13:31:13 +0100

    Add Get-MyDefenderSummary function to summarize Defender health issues
    - Introduced Get-MyDefenderSummary function to aggregate open health issues
    - Categorized issues into global and sensor-related for better reporting

---
+++ Public/Get-MyDefenderSummary.ps1
@@ -0,0 +1,39 @@
+﻿function Get-MyDefenderSummary {
+    [cmdletbinding()]
+    param(
+
+    )
+
+    $DefenderHealthIssues = Get-MyDefenderHealthIssues -Status 'open'
+
+    $ReportInformation = @{
+        GlobalIssues = [System.Collections.Generic.List[PSCustomObject]]::new()
+        SensorIssues = [System.Collections.Generic.List[PSCustomObject]]::new()
+    }
+
+    foreach ($Issue in $DefenderHealthIssues) {
+        if ($Issue.HealthIssueType -eq 'sensor') {
+            $ReportInformation.SensorIssues.Add($Issue)
+        } else {
+            $ReportInformation.GlobalIssues.Add($Issue)
+        }
+    }
+
+    if ($Summary) {
+        $CachedOutput = [ordered]@{}
+        foreach ($Issue in $Output) {
+            if (-not $CachedOutput[$Issue.DisplayName]) {
+                $CachedOutput[$Issue.DisplayName] = [ordered] @{}
+            }
+            $CachedOutput[$Issue.DisplayName]
+
+            if ($Issue.SensorDNSName) {
+                $CachedOutput[$Issue.SensorDNSName]
+            } else {
+
+            }
+        }
+    } else {
+        $Output
+    }
+}
