ebe7b917eadb086e0f547976b766964ab81990ee
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-03-03 09:04:50 +0100

    Add ShowConditionalAccess.ps1 example script
    - Import GraphEssentials module
    - Connect to Microsoft Graph with necessary scopes
    - Include commented-out examples for retrieving conditional access data
    - Call Show-MyConditionalAccess to generate a report

---
+++ Examples/ShowConditionalAccess.ps1
@@ -0,0 +1,11 @@
+﻿Import-Module .\GraphEssentials.psd1 -Force
+
+Connect-MgGraph -Scopes 'Policy.Read.All', Application.ReadWrite.All, AccessReview.Read.All, AdministrativeUnit.Read.All, 'User.Read.All', RoleManagement.Read.Directory, Directory.Read.All, EntitlementManagement.Read.All
+
+# $ConditionalAccess = Get-MyConditionalAccess -Verbose
+# $ConditionalAccess | Format-Table
+
+# $ConditionalAccessStatistics = Get-MyConditionalAccess -Verbose -IncludeStatistics
+# $ConditionalAccessStatistics | Format-Table
+
+Show-MyConditionalAccess -Verbose -FilePath $PSScriptRoot\Reports\ConditionalAccess.html -Online -ShowHTML
