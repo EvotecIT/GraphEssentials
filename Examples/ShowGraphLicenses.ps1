c268c5bd2ed0338d5fd12d7aa6661f22fda82ca9
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-03-03 09:05:01 +0100

    Add Invoke-MyGraphEssentials call to ShowGraphLicenses.ps1
    - Include invocation for Roles, RolesUsers, and RolesUsersPerColumn
    - Maintain existing functionality for Users and license-related data

--- Examples/ShowGraphLicenses.ps1
+++ Examples/ShowGraphLicenses.ps1
@@ -1,5 +1,6 @@
 ﻿Import-Module .\GraphEssentials.psd1 -Force
 
 Connect-MgGraph -Scopes 'User.Read.All', Directory.Read.All
+Invoke-MyGraphEssentials -Type Roles, RolesUsers, RolesUsersPerColumn -FilePath $PSScriptRoot\Reports\GraphEssentials.html -Verbose #-SplitReports
 Invoke-MyGraphEssentials -Type Users -FilePath $PSScriptRoot\Reports\GraphEssentials.html -Verbose #-SplitReports
 Invoke-MyGraphEssentials -Type Users, UsersPerLicense, UsersPerServicePlan, Licenses -FilePath $PSScriptRoot\Reports\GraphEssentials.html -Verbose #-SplitReports
