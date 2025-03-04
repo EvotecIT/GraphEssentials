256036548748dd3cc2a339c78542ed203e75b8f7
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-03-04 10:07:48 +0100

    Add Get-AuthMethodConfig.ps1 for retrieving authentication method configurations
    - Implemented a function to get configuration for a specified authentication method.
    - Added error handling to manage failures during configuration retrieval.

---
+++ Private/Get-AuthMethodConfig.ps1
@@ -0,0 +1,15 @@
+﻿function Get-AuthMethodConfig {
+    [CmdletBinding()]
+    param (
+        [string] $MethodName,
+        [string] $ConfigId
+    )
+    try {
+        Write-Verbose -Message "Get-MyAuthenticationMethodsPolicy - Getting configuration for $MethodName"
+        $Config = Get-MgPolicyAuthenticationMethodPolicyAuthenticationMethodConfiguration -AuthenticationMethodConfigurationId $ConfigId -ErrorAction Stop
+        return $Config
+    } catch {
+        Write-Warning -Message "Get-MyAuthenticationMethodsPolicy - Failed to get configuration for $MethodName. Error: $($_.Exception.Message)"
+        return $null
+    }
+}
