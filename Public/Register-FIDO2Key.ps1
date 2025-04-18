fec89d4b941d41ca74fd063c45583e8eaf22d350
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-04-18 10:50:37 +0200

    feat(Register-FIDO2Key): ✨ Add module availability check for 'DSInternals.Passkeys'
    * Implemented a check to ensure the 'DSInternals.Passkeys' module is installed before proceeding with FIDO2 key registration.
    * Added a warning message to guide users on installation if the module is missing.

--- Public/Register-FIDO2Key.ps1
+++ Public/Register-FIDO2Key.ps1
@@ -5,6 +5,11 @@
         [string]$DisplayName = "YubiKey",
         [switch]$PassThru
     )
+    if (-not (Get-Module -ListAvailable -Name 'DSInternals.Passkeys' -ErrorAction SilentlyContinue)) {
+        Write-Warning -Message "DSInternals.Passkeys module is not installed. Please install it from the PowerShell Gallery."
+        return
+    }
+
     $FIDO2Options = Get-PasskeyRegistrationOptions -UserId $UPN -ErrorAction Stop
     $FIDO2 = New-Passkey -Options $FIDO2Options -DisplayName $DisplayName -ErrorAction Stop
     if ($FIDO2) {
