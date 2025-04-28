66c8cc8269fcc3a2b968947df36e19848f8b7c82
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-04-28 20:18:05 +0200

    chore(ShowGraph): 🔧 Update script to enable device retrieval
    * Uncommented `Get-MyDevice` to allow device information retrieval.
    * Ensured all `Invoke-MyGraphEssentials` calls are active for comprehensive data collection.

--- Examples/ShowGraph.ps1
+++ Examples/ShowGraph.ps1
@@ -2,9 +2,9 @@
 
 Connect-MgGraph -Scopes Application.ReadWrite.All, AccessReview.Read.All, AdministrativeUnit.Read.All, 'User.Read.All', RoleManagement.Read.Directory, Directory.Read.All, EntitlementManagement.Read.All
 
-#Invoke-MyGraphEssentials -Type DevicesIntune, Roles, RolesUsers, RolesUsersPerColumn #, Apps, AppsCredentials -FilePath $PSScriptRoot\Reports\GraphEssentials.html -Verbose #-SplitReports
-#Invoke-MyGraphEssentials -Type RolesUsersPerColumn -FilePath $PSScriptRoot\Reports\GraphEssentials.html -Verbose #-SplitReports
-#Invoke-MyGraphEssentials -Type Devices, DevicesIntune, Users -FilePath $PSScriptRoot\Reports\GraphEssentials.html -Verbose #-SplitReports
+Invoke-MyGraphEssentials -Type DevicesIntune, Roles, RolesUsers, RolesUsersPerColumn #, Apps, AppsCredentials -FilePath $PSScriptRoot\Reports\GraphEssentials.html -Verbose #-SplitReports
+Invoke-MyGraphEssentials -Type RolesUsersPerColumn -FilePath $PSScriptRoot\Reports\GraphEssentials.html -Verbose #-SplitReports
+Invoke-MyGraphEssentials -Type Devices, DevicesIntune, Users -FilePath $PSScriptRoot\Reports\GraphEssentials.html -Verbose #-SplitReports
 
-#Get-MyDevice -Synchronized | Format-Table
+Get-MyDevice -Synchronized | Format-Table
 Get-MyDeviceIntune -Synchronized | Format-Table
