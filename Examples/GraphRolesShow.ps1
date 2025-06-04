51b54cc42cdcb70c011aace0f09ea2048219e7dd
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-04 22:02:06 +0200

    feat(GraphRolesShow): ✨ Add script to generate HTML report for Azure AD role management
    * Introduced `GraphRolesShow.ps1` to connect to Microsoft Graph and generate a comprehensive HTML report of Azure AD roles and assignments.
    * Integrated necessary permissions for role management and auditing, enhancing reporting capabilities for administrators.

---
+++ Examples/GraphRolesShow.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module .\GraphEssentials.psd1 -Force
+
+Connect-MgGraph -Scopes @(
+    "RoleManagement.Read.Directory", "RoleManagement.Read.All", 'RoleAssignmentSchedule.Read.Directory', "Directory.Read.All", "AuditLog.Read.All"
+    "RoleAssignmentSchedule.ReadWrite.Directory", "RoleManagement.ReadWrite.Directory", "RoleAssignmentSchedule.Remove.Directory"
+) -NoWelcome
+
+# Generate comprehensive HTML report combining all role functions
+Show-MyRole -FilePath "$PSScriptRoot\RoleManagementReport.html" -Online -DaysBack 90 -Verbose -Show
