d21b82663235cb6c012bef47e55ff5ee11c6f3d6
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2026-09-12 23:14:50 +0200

    Keep rolling module build version

--- Build/Build-Module.ps1
+++ Build/Build-Module.ps1
@@ -3,7 +3,7 @@
 Invoke-ModuleBuild -ModuleName 'GraphEssentials' {
     # Usual defaults as per standard module
     $Manifest = [ordered] @{
-        ModuleVersion        = '0.0.61'
+        ModuleVersion        = '0.0.X'
         CompatiblePSEditions = @('Desktop', 'Core')
         GUID                 = '75ef812f-6d8e-4898-81bb-8029e0560ef3'
         Author               = 'Przemyslaw Klys'
